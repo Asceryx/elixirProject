151bd1353ab5cf33c492b9628b39e01249968f8e
Bro-epitech <67640536+Bro-epitech@users.noreply.github.com>
2020-10-20 13:33:54 +0200

    Update ClocksController.ex

--- lib/project_web/controllers/ClocksController.ex
+++ lib/project_web/controllers/ClocksController.ex
@@ -1,7 +1,13 @@
 defmodule ProjectWeb.ClocksController do
   use ProjectWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
+def getById(conn, %{"clocksID" => clocksID}) do
+    try do
+      clock = Repo.get!(Clocks, clocks)
+      conn
+      |> put_status(200) #requête avec succès  
+      |> json(%{"id" => clock.id, "status" => clock.status, "time" => clock.time})
+    rescue
+      Ecto.NoResultsError -> json(conn, 400)#requête avec echec 
+    end
   end
 end
