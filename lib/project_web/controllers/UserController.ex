7abd400e2c8991ade236480c722c75a313cf0d7a
Pivil <remi.pontonnier.38@gmail.com>
2020-10-20 11:45:26 +0200

    adding getById and add to user controller

--- lib/project_web/controllers/UserController.ex
+++ lib/project_web/controllers/UserController.ex
@@ -6,18 +6,50 @@ defmodule ProjectWeb.UserController do
   def getById(conn, %{"userID" => userID}) do
     try do
       user = Repo.get!(Users, userID)
-      json(conn, %{user: user})
+      conn
+      |> put_status(200)
+      |> json(%{"id" => user.id, "username" => user.username, "email" => user.email})
     rescue
       Ecto.NoResultsError -> json(conn, 400)
     end
   end
 
-  def add(conn, params) do
-    json(conn, %{body: params})
+  def add(conn, %{"email" => email, "username" => username}) do
+    changeset =
+      Users.changeset(%Users{}, %{"email" => email, "username" => username})
+
+    case Repo.insert!(changeset) do
+      {:ok, _user} ->
+        json(conn |> put_status(:created), %{success: "User created"})
+
+      {:error, changeset} ->
+        json(conn, 400)
+    end
+
+    conn
+    |> put_status(201)
+    |> json(%{"response" => "create"})
   end
 
-  def edit(conn, params) do
-    json(conn, %{body: params})
+  def edit(conn, %{"id" => id, "email" => email, "username" => username}) do
+    user = Repo.get(Users, id)
+
+    changeset = 
+      Users.changeset(%Users{}, %{"email" => email, "username" => username})
+    |> change(%{ username: "New title" })
+    
+    json(conn, 200)
+    # case Repo.insert!(changeset) do
+    #   {:ok, _user} ->
+    #     json(conn |> put_status(:created), %{success: "User created"})
+
+    #   {:error, changeset} ->
+    #     json(conn, 400)
+    # end
+
+    # conn
+    # |> put_status(201)
+    # |> json(%{"response" => "create"})
   end
 
   def delete(conn, params) do
